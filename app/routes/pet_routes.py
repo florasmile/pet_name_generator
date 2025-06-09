a5882eb7dc82c59f51d408982816301ca2a03e74
dehuihu <dehuihu@gmail.com>
2025-06-09 11:25:57 -0700

    Add generate_petname helper and post routes(still debugging)

--- app/routes/pet_routes.py
+++ app/routes/pet_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, abort, make_response
 from ..db import db
 from ..models.pet import Pet
 from google import genai
-
+import os
 
 client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 bp = Blueprint("pets", __name__, url_prefix="/pets")
@@ -10,8 +10,30 @@ bp = Blueprint("pets", __name__, url_prefix="/pets")
 
 @bp.post("")
 def create_pet():
-    pass
+    pet_data = generate_pet_name()
+    pet_obj = Pet.from_dict(pet_data)
+    db.session.add(pet_obj)
+    db.session.commit()
 
+    return pet_obj.to_dict()
+
+def generate_pet_name():
+    # privide the input message for AI API
+    type = "cat"
+    personality = "cute"
+    color = "orange"
+    input_message = f"I need to come up with a name for a pet. The animal type is {type}, the personality is {personality}, and color is {color}. Please help generate a pet name based on those attributes."
+    # call client method to get response
+    response = client.models.generate_content(model="gemini-1.5-flash", contents=input_message)
+
+    print(response.text)
+
+    return {
+        "name":{response.text},
+        "animal": {type},
+        "personality": {personality},
+        "coloration": {color}
+    }
 @bp.get("")
 def get_pets():
     pet_query = db.select(Pet)
